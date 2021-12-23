58a5a6b1d6b6de3d1c3d5a79b4ebc30e289e9982
kevinm <mbongo.kevin.pro@gmail.com>
2021-12-23 04:19:22 +0100

    feat(project): create html and style

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,31 @@
 <template>
-  <div id="app">Hello World</div>
+  <div id="app">
+    <article class="flex flex-col md:flex-row w-screen h-screen">
+      <section class="bg-red-600 hover:bg-red-700" />
+      <section class="bg-yellow-600 hover:bg-yellow-700" />
+      <section class="bg-yellow-400 hover:bg-yellow-500" />
+      <section class="bg-green-600 hover:bg-green-700" />
+      <section class="bg-blue-600 hover:bg-blue-700" />
+      <section class="bg-purple-600 hover:bg-purple-700" />
+      <section class="bg-pink-600 hover:bg-pink-700" @click="playSound" />
+    </article>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'App',
+
+  methods: {
+    playSound() {
+      console.log('coucou')
+    },
+  },
 }
 </script>
+
+<style scoped>
+section {
+  @apply transition-all duration-300 ease-in-out flex-1 cursor-pointer;
+}
+</style>
