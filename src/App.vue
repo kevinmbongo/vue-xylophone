0f0e359fa327ce8e4134dde6764b2678e0c1887a
kevinm <mbongo.kevin.pro@gmail.com>
2021-12-24 20:58:22 +0100

    feat(project): create VDrawer component and implement on click feature

--- src/App.vue
+++ src/App.vue
@@ -2,64 +2,73 @@
   <div id="app">
     <article class="flex flex-col md:flex-row w-screen h-screen">
       <piano-key
-        v-for="item in items"
+        v-for="item in instrumentDataBase[currentInstrumentIndex].keys"
         :key="item.noteUrl"
         :note-url="item.noteUrl"
         :color="item.color"
       />
+      <v-button-drawer @click="drawerIsOpen = true" />
+      <v-drawer />
     </article>
   </div>
 </template>
 
 <script>
-import PianoKey from '@/components/core/PianoKey'
+import PianoKey from '@/components/core/VPianoKey'
+import VButtonDrawer from '@/components/core/VButtonDrawer'
+import VDrawer from '@/components/core/VDrawer'
 
 export default {
   name: 'App',
 
-  components: { PianoKey },
+  components: { VDrawer, VButtonDrawer, PianoKey },
 
-  computed: {
-    items() {
-      return [
-        {
-          noteUrl: require('@/assets/sounds/note7.wav'),
-          color: '#dc3030',
-          accentColor: '',
-          keyBordKey: 'S',
-        },
-        {
-          noteUrl: require('@/assets/sounds/note6.wav'),
-          color: '#6fdc30',
-          accentColor: '',
-        },
-        {
-          noteUrl: require('@/assets/sounds/note5.wav'),
-          color: '#30a8dc',
-          accentColor: '',
-        },
-        {
-          noteUrl: require('@/assets/sounds/note4.wav'),
-          color: '#dc3078',
-          accentColor: '',
-        },
-        {
-          noteUrl: require('@/assets/sounds/note3.wav'),
-          color: '#dcb430',
-          accentColor: '',
-        },
-        {
-          noteUrl: require('@/assets/sounds/note2.wav'),
-          color: '#dc6c30',
-          accentColor: '',
-        },
-        {
-          noteUrl: require('@/assets/sounds/note1.wav'),
-          color: '#6130dc',
-          accentColor: '',
-        },
-      ]
-    },
-  },
+  data: () => ({
+    drawerIsOpen: false,
+    currentInstrumentIndex: 0,
+    instrumentDataBase: [
+      {
+        id: 'xylophon',
+        keys: [
+          {
+            noteUrl: require('@/assets/sounds/note7.wav'),
+            color: '#dc3030',
+            accentColor: '',
+            keyBordKey: 'S',
+          },
+          {
+            noteUrl: require('@/assets/sounds/note6.wav'),
+            color: '#6fdc30',
+            accentColor: '',
+          },
+          {
+            noteUrl: require('@/assets/sounds/note5.wav'),
+            color: '#30a8dc',
+            accentColor: '',
+          },
+          {
+            noteUrl: require('@/assets/sounds/note4.wav'),
+            color: '#dc3078',
+            accentColor: '',
+          },
+          {
+            noteUrl: require('@/assets/sounds/note3.wav'),
+            color: '#dcb430',
+            accentColor: '',
+          },
+          {
+            noteUrl: require('@/assets/sounds/note2.wav'),
+            color: '#dc6c30',
+            accentColor: '',
+          },
+          {
+            noteUrl: require('@/assets/sounds/note1.wav'),
+            color: '#6130dc',
+            accentColor: '',
+          },
+        ],
+      },
+    ],
+  }),
 }
 </script>
---
+++ src/components/core/VDrawer.vue
@@ -0,0 +1,32 @@
+<template>
+  <nav>
+    <button>
+      <img src="@/assets/images/guitar.png" alt="guitar" />
+      <p>Guitar</p>
+    </button>
+    <button>
+      <img src="@/assets/images/piano.png" alt="piano" />
+      <p>Piano</p>
+    </button>
+    <button>
+      <img src="@/assets/images/xylophone.png" alt="xylophone" />
+      <p>Xylophone</p>
+    </button>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'VDrawer',
+}
+</script>
+
+<style scoped>
+button {
+  @apply flex items-center m-5;
+}
+
+p {
+  @apply mx-5;
+}
+</style>
